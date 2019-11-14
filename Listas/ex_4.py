60ccf873e55b5b6bcbfb8fc71138c13ab142b6c0
arthurferro <arthurdias.etec@gmail.com>
2019-11-14 20:58:11 -0300

    Added ex_6.py in Listas

--- Listas/ex_4.py
+++ Listas/ex_4.py
@@ -1,16 +1,17 @@
-##Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.##
+##Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas.
+## Imprima as consoantes.
+
 vetor = []
 vogais = ['a','e','i','o','u']
 consoantes = []
 for i in range(1,11):
     letra = input("Digite uma letra: ")
-    letra = letra.lower
-    if letra in vogais:
+    if letra.lower() in vogais:
         vetor.append(letra)
     else:
         vetor.append(letra)
         consoantes.append(letra)
 
 print(f"Todas as letras digitadas: {vetor}")
-print(f"Quantidade de consoantes: {sum(consoantes)}")
+print(f"Quantidade de consoantes: {len(consoantes)}")
 print(f"Todas as consoantes: {consoantes}")
---
+++ Listas/ex_6.py
@@ -0,0 +1,29 @@
+#Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
+## imprima o número de alunos com média maior ou igual a 7.0.#
+import os
+from time import sleep
+
+os.system('cls' if os.name == 'nt' else 'clear')
+mediaNotasAlunos = []
+mediaAluno = []
+qtdMediaMaior = float(input("Digite a média suficiente de cada aluno: "))
+nAlunosMedia = 0
+print("Vamos começar...")
+sleep(2)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+for a in range (1,3):
+    for n in range(1,5):
+        nota = float(input(f"Digite a {n}º nota do {a}º aluno: "))
+        mediaAluno.append(nota)
+    print("Aguarde...")
+    sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    mediaNotasAlunos.append(sum(mediaAluno)/len(mediaAluno))
+    
+
+for media in mediaNotasAlunos:
+    if media >= qtdMediaMaior:
+        nAlunosMedia+=1
+
+print(f"Número de alunos com média maior que {qtdMediaMaior}: {nAlunosMedia} aluno(s)")
